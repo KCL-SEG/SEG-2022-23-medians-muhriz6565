ee5937db3f39f803d58b008de7ce39601a10cd0f
Muhriz Tauseef <muhriztauseef@gmail.com>
2022-10-07 11:52:06 +0100

    Python exercise 1, complete

--- median.py
+++ median.py
@@ -1,6 +1,5 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +8,23 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+
+def isEven(numbers):
+    if len(numbers)%2==0:
+        evenList(numbers)
+    else:
+        oddList(numbers)
+def evenList(numbers):
+    half=int(len(numbers)/2)
+    to_mean=(numbers[half-1],numbers[half])
+    mean(to_mean)
+
+def mean(tup):
+    print (sum(tup)/2)
+
+def oddList(numbers):
+    print(numbers[len(numbers)//2])
+
+isEven(numbers)
